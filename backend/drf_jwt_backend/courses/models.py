540241798e8b6c870dc4a411002f5ba332637826
caitevanish <cait.evan@gmail.com>
2022-04-20 19:10:30 -0700

    Backend: Revising Courses model. Modified classes Course and StudyTimeTable. Added classes Purchase Type, Company, and CourseWishlist. Made migrations and sent to db. Issue: CourseWishList not present in workbench.

--- backend/drf_jwt_backend/courses/models.py
+++ backend/drf_jwt_backend/courses/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from projects.models import Project
+
 # Create your models here.
 
 class Course(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE)
   title = models.CharField(max_length=100)
-  company = models.CharField(max_length=100, null=True, blank=True)
-  price = models.IntegerField()
+  price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+  company = models.ForeignKey('Company', on_delete=models.PROTECT)
+  purchase_type = models.OneToOneField('PurchaseType', on_delete=models.PROTECT, primary_key=True) 
   purchase_date = models.DateField(null=True, blank=True)
-  purchase_type = models.CharField(max_length=30,null=True, blank=True) # If I only have a couple choices to offer, what's the correct datafield type?
-  # project = models.ManyToManyField("Project", many=True, blank=True, null=True, on_delete=models.CASCADE)
+  projects = models.ManyToManyField(Project, related_name='projects')
   is_active = models.BooleanField(default=True)
   help_me_to = models.TextField(max_length=3000, blank=True, null=True)
   be_great = models.TextField(max_length=3000, blank=True, null=True)
@@ -18,14 +20,52 @@ class Course(models.Model):
   def __str__(self):
     return self.title
 
+class PurchaseType(models.Model):
+  TYPE_CHOICES = [
+    ('SP', 'Single Payment'),
+    ('MS', 'Month Subscription'),
+    ('AS', 'Annual Subscription'),
+    ('PP', 'Payment Plan'),
+  ] 
+  type = models.CharField(max_length=2, choices=TYPE_CHOICES, default= 'SP')
+  is_one_time_pay = models.BooleanField(default=True)
+
+  def __str__(self):
+    return self.type
+
+class Company(models.Model):
+  name = models.CharField(max_length=100)
+  url = models.TextField(max_length=100, null=True, blank=True)
+
+  def __str__(self):
+    return self.name
+
+class CourseWishlist(models.Model):
+  title = models.CharField(max_length=100)
+  url = models.TextField(max_length=100, null=True, blank=True)
+  company = models.ForeignKey(Company, on_delete=models.CASCADE)
+  price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+  why_not_now = models.TextField(max_length=3000, blank=True, null=True)
+  notes = models.TextField(max_length=3000, blank=True, null=True)
+
+  def __str__(self):
+    return self.title
+
+
 class StudyTimetable(models.Model):
+  course = models.OneToOneField(Course, on_delete=models.CASCADE, primary_key=True)
+  date_logged = models.DateTimeField(auto_now_add=True)
+  time_start = models.DateTimeField(auto_now_add=True)
+  time_end = models.DateTimeField(auto_now=True)
 
-  #Make choices for category of projects (personal, professional, etc.)
 
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
-  date_logged = models.DateField(null=True, blank=True)
-  time_start = models.TimeField(auto_now=False, default=False, editable=True)
-  time_end = models.TimeField(auto_now=False, default=False, editable=True)
-  course = models.ForeignKey(Course, blank=True, null=True, on_delete=models.CASCADE)
+#Junction Tables
 
-#Do I include string overflow?
+# class UserCourse(models.Model):
+#   course = models.ForeignKey(Course, on_delete=models.CASCADE)
+#   user =  models.ForeignKey(User, on_delete=models.CASCADE) 
+
+# class CourseProject(models.Model):
+#   course = models.ForeignKey(Course, on_delete=models.CASCADE)
+#   project = models.ForeignKey(Project, on_delete=models.CASCADE)
+#   user = models.ForeignKey(User, on_delete=models.CASCADE) 
--- backend/drf_jwt_backend/projects/models.py
+++ backend/drf_jwt_backend/projects/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from courses.models import Course
+# from courses.models import Course
 from django.contrib.auth.models import User
 
 # Create your models here.
